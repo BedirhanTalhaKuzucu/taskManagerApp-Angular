2da0debb8ad27c26fb4c3d7174d7e9dcf3a39baf
Bedirhan Kuzucu <btk.kzc@gmail.com>
2024-04-12 19:49:08 +0200

    new form added and refactored

--- src/app/core/component/task-form/task-form.component.ts
+++ src/app/core/component/task-form/task-form.component.ts
@@ -4,30 +4,28 @@ import { TasklistService } from '../../services/tasklist.service';
 @Component({
   selector: 'app-task-form',
   templateUrl: './task-form.component.html',
-  styleUrls: ['./task-form.component.css']
+  styleUrls: ['./task-form.component.css'],
 })
 export class TaskFormComponent {
+  @Output() getNewTask = new EventEmitter<any>();
 
-  @Output() getNewTask = new EventEmitter<any>
-
-
-  constructor( private fb : FormBuilder, public service : TasklistService  ){}
+  constructor(private fb: FormBuilder, public service: TasklistService) {}
 
   getTaskDetail = this.fb.group({
-    text:['', [Validators.required]],
-    day:['', [Validators.required]],
-  })
+    text: ['', [Validators.required]],
+    date: [{ day: 0, month: 0, year: 0 }, [Validators.required]],
+    description: [''],
+  });
 
-  formSubmit(){
+  formSubmit() {
     let data = {
-      text: this.getTaskDetail.value.text,
-      day: this.getTaskDetail.value.day,
-      isDone:false
+      ...this.getTaskDetail.value,
+      date: `${this.getTaskDetail?.value?.date?.day}/${this.getTaskDetail?.value?.date?.month}/${this.getTaskDetail?.value?.date?.year}`,
+      isDone: false,
     };
-    this.service.createTask("/tasks", data ).subscribe((response)=>{
-      this.getNewTask.emit(response)
-      this.getTaskDetail.reset()
-    })
+    this.service.createTask('/tasks', data).subscribe((response) => {
+      this.getNewTask.emit(response);
+      this.getTaskDetail.reset();
+    });
   }
-
 }
