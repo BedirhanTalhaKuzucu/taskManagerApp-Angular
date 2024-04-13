0926419d318f9fe8bbfd3e1f78bf69ee418e552d
Bedirhan Kuzucu <btk.kzc@gmail.com>
2024-04-13 15:54:02 +0200

    usused import deleted

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './component/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserModule } from '@angular/platform-browser';
 import { HighlightDirective } from './directives/highlight.directive';
 import { RouterModule } from '@angular/router';
 
